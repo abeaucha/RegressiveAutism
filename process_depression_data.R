b889ef7b11099b30f75a43118e49922ac9fe0452
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2026-03-31 16:41:50 -0400

    Depression processing script done

--- process_depression_data.R
+++ process_depression_data.R
@@ -15,28 +15,23 @@ suppressPackageStartupMessages(library(readxl))
 # Main ------------------------------------------------------------------------
 
 # Paths to anxiety directories
-input_dir <- "data/raw/Anxiety/"
-output_dir <- "data/processed/Anxiety/"
+input_dir <- "data/raw/Depression/"
+output_dir <- "data/processed/Depression/"
 
 # Data frame containing files and threshold info
-df_files <- tibble(file = c("AGRE_AFFCHILD1_ANXIETY_AGE_AT_DIAGNOSIS.xlsx",
-                            "AGRE_AFFCHILD1_ANXIETY_SYMPTOMS.xlsx",
-                            "CBCL1_CB2TS.xlsx",
-                            "CBCL618_CB682TS; Tscores.xlsx",
-                            "CCDC_CDCDBOSY.xlsx",
-                            "CCDC_CDCDPDX.xlsx",
-                            "CLINICALINFO_ASDANXIET.xlsx",
-                            "RCADSP_GA_TSCORE.xlsx",
-                            "SPENCEP_SPENCE_TOT_SCORE; SCAS (Jan 29, 2026 12-16-51 PM).xlsx"),
-                   threshold = c(0, 1, 65, 65, 1, 1, 1, 65, 65),
-                   comparison = c(">=", "=", ">=", ">=", "=", "=", "=", ">=", ">="),
-                   colname = c("AGRE_AGE", "AGRE", "CBCL1", "CBCL618", "CDCDBOSY",
-                               "CDCDPDX", "ASDANXIETY", "RCADSP", "SPENCEP")) %>% 
+df_files <- tibble(file = c("AGRE_AFFCHILD1_DEPRESSION_AGE_AT_DIAGNOSIS.xlsx", 
+                             "AGRE_AFFCHILD1_DEPRESSION_DIAGNOSIS.xlsx",
+                             "AGRE_AFFCHILD1_DEPRESSIVE_SYMPTOMS.xlsx",
+                            "CBCL1_CB1TS 1-5yold.xlsx",
+                            "CBCL618_CB681TS 6-18y old Affective Problems.xlsx",
+                            "CCDC_CDCOT2SY Depression Diagnosis.xlsx",
+                            "RCADSP_D_TSCORE.xlsx"),
+                   threshold = c(0, 1, 1, 70, 70, 1, 70),
+                   comparison = c(">=", "=", "=", ">=", ">=", "=", ">="),
+                   colname = c("AGRE_AGE", "AGRE_DX", "AGRE_SY",
+                               "CBCL1", "CBCL618", "CCDC","RCADSP")) %>% 
   mutate(colname = paste0(colname, "_PASS"))
 
-# Remove 
-df_files <- df_files %>%
-  filter(!(colname %in% c("CDCDBOSY", "CDCDPDX")))
 
 for (i in 1:nrow(df_files)) {
   
@@ -66,33 +61,25 @@ for (i in 1:nrow(df_files)) {
   colnames(df)[2] <- df_files[[i, "colname"]]
   
   if (i == 1) {
-    df_anxiety <- df
+    df_depression <- df
   } else {
-    df_anxiety <- full_join(df_anxiety, df, by = "ID")
+    df_depression <- full_join(df_depression, df, by = "ID")
   }
   
 }
 
 # Calculate combined anxiety score
-df_anxiety_combined <- df_anxiety %>% 
+df_depression_combined <- df_depression %>% 
   column_to_rownames("ID") %>% 
   apply(1, function(x) {any(as.logical(x), na.rm = TRUE)}) %>% 
-  enframe(name = "ID", value = "Anxiety_PASS")
+  enframe(name = "ID", value = "Depression_PASS")
 
 # Join combined score to individual scores
-df_anxiety <- df_anxiety %>% 
-  left_join(df_anxiety_combined, by = "ID")
+df_depression <- df_depression %>% 
+  left_join(df_depression_combined, by = "ID")
 
 # Export processed data
-outfile <- "anxiety.csv"
+outfile <- "depression.csv"
 outfile <- file.path(output_dir, outfile)
-write_csv(df_anxiety_combined, file = outfile)
-
-
-# Note that false here doesn't mean they don't have ADHD. 
-
-# No participants have all variables, so can't define a proper negative case
-# mat_ADHD <- as.matrix(df_ADHD)
-# mat_ADHD_na <- is.na(mat_ADHD)
-# which(rowSums(mat_ADHD_na) == 0)
+write_csv(df_depression_combined, file = outfile)
 
